85b02e490fbd652092527b2732a6ff6bb7058bb4
Tine Kleivane <kleivane@gmail.com>
2014-10-01 20:41:21 +0200

    Initial dotfiles

---
+++ README.md
@@ -0,0 +1,10 @@
+Install XCode from App Store, forsøk git i terminalen
+Install [Homebrew](http://brew.sh)
+Install [iTerm2](http://iterm2.com/downloads.html)
+
+Install “Oh My Zsh”
+
+Clone git@github.com:kleivane/dotfiles.git
+
+Run `sh installConfig.sh`
+
---
+++ installConfig.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Brew packages 
+#
+brew install node
+brew install wget
+brew install zsh
+
+#
+# Install Cask itself
+#
+brew install caskroom/cask/brew-cask
+
+
+#
+# Cask packages 
+#
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install vlc
+brew cask install slack
