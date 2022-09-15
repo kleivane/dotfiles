e40588fa13f83fb0365df4c944502f851c39cc08
Tine Kleivane <kleivane@gmail.com>
2022-09-15 10:56:56 +0200

    Update installConfig.sh

--- installConfig.sh
+++ installConfig.sh
@@ -2,22 +2,27 @@
 #
 # Brew packages 
 #
+
 brew install node
 brew install wget
 brew install tree
 brew install aws-vault
+brew install terraform
+
 #
 # Install Cask itself
 #
+
 brew install caskroom/cask/brew-cask
 
 #
 # Cask packages 
 #
+
 brew cask install dropbox
 brew cask install atom
 brew cask install intellij-idea
 brew cask install spotify
 brew cask install postman
-#brew cask install google-chrome
-#brew cask install slack
+brew cask install google-chrome
+brew cask install slack
